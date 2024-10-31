d482c4229a89c7b66059bf75f92665db8d8a5ab8
Azeezsolola <oloyedeifeoluwa21@gmail.com>
2024-10-31 00:57:13 -0400

    AWS-52: Modified depoloyent using account mapping stack_Azeez_Solola (#9)

--- Provider.tf
+++ Provider.tf
@@ -2,12 +2,9 @@ provider "aws" {
   region = var.AWS_REGION
 #access_key = var.AWS_ACCESS_KEY
 #secret_key = var.AWS_SECRET_KEY
-
 assume_role {
-  
-  role_arn = "arn:aws:iam::ACCOUNTS["var.environment"]:role/Engineer"
+  #The role ARN within Account B to AssumeRole into. Created in step 1.
+  #role_arn = "arn:aws:iam::var.ACCOUNTS[var.environment]:role/Engineer"
+  role_arn = local.account_arn
   }
 }
-
-
-
